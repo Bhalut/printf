4be5526562b458fd8323a71a4632be5e941cdcf6
Abdel Mejia <bhalut.bear@gmail.com>
2020-03-16 19:16:03 +0000

    Add struct in header file

--- holberton.h
+++ holberton.h
@@ -1,7 +1,21 @@
 #endif HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 
-#endif
+/**
+ * fmt - struct f_index type
+ * struct formats - functions for printf
+ * @index: index by function
+ * @func: pointer to function - funtion print
+ */
+typedef struct formats
+{
+	char *index;
+	int (*func)(va_list arg);
+} fmt;
+
+#endif /* HOLBERTON_H */
