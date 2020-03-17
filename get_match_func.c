8a8244ad6ed3df6b94c8b0224f7f5d0623a13c2f
erikaosgue <1476@holbertonschool.com>
2020-03-17 22:10:00 +0000

    Fix the % case options to print

--- get_match_func.c
+++ get_match_func.c
@@ -8,31 +8,19 @@
 int get_match_func(const char *format, va_list list)
 {
 	int count = 0, i = 0, j, k = 0, num = 0;
-	fmt st_format[] = {{"c", func_char}, {"s", func_string},
-			   {"d", func_digit}, {"i", func_digit},
-			   {"%", func_percent}, {NULL, NULL}};
+	fmt st_format[] = {{"c", func_char}, {"s", func_string}, {"d", func_digit},
+		{"i", func_digit}, {"%", func_percent}, {NULL, NULL}};
 
-	while (format && format[i] != 0)
+	for (i = 0; format && format[i] != 0; i++)
 	{
 		if (format[i] != '%')
 		{
-			if (format[i] == '\n')
-			{
-				_putchar('\n');
-				i++;
-				count += 1;
-				break;
-			}
-			else
-			{
-				_putchar(format[i]);
-				count += 1;
-			}
+			_putchar(format[i]);
+			count += 1;
 		}
 		else
 		{
-			j = 0;
-			while (st_format[j].fmt)
+			for (j = 0; st_format[j].fmt; j++)
 			{
 				if (format[i + 1] == st_format[j].fmt[k])
 				{
@@ -41,10 +29,21 @@ int get_match_func(const char *format, va_list list)
 					i++;
 					break;
 				}
-				j++;
+			}
+			if (st_format[j].fmt == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != 0)
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					i++;
+				}
+				else
+					return (-1);
 			}
 		}
-		i++;
 	}
+	if (format == NULL)
+		return (-1);
 	return (count);
 }
